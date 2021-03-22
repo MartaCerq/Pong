a86f9a5312a3a331370d3030d9b103c084304386
MartaCerq <79975159+MartaCerq@users.noreply.github.com>
2021-03-22 19:34:13 +0000

    Initial commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 0.5f;
+
+    [SerializeField]
+    float paddleHeight = 1f;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+
+        /*
+         *  se tecla para cima permida
+         *  então subir
+         *  senão se tecla para baixo premida 
+         *  então descer
+         * 
+         * 
+         */
+
+        /* 
+         * mover a raquete
+         */
+
+        if (Input.GetKey(upKey))
+        {
+
+            transform.position += velocidade * Vector3.up * Time.deltaTime;
+
+            // sobe 
+        }
+
+        else if (Input.GetKey(downKey))
+
+        {
+            transform.position += velocidade * Vector3.down * Time.deltaTime;
+
+            // desce 
+        }
+
+        /*
+         * verificar os limites ( com ifs)
+         *
+
+        float cameraHeight = Camera.main.orthographicSize;
+
+
+        if(transform.position.y >= cameraHeight - 0.5f )
+        {
+            // variável para poder alterar o valor de .y no transform.position
+
+            Vector3 positionAux = transform.position;
+            positionAux.y = cameraHeight - 0.5f;
+            transform.position = positionAux;
+
+        }
+
+        if ( transform.position.y <= -cameraHeight + 0.5f)
+        {
+            Vector3 positionAux = transform.position;
+            positionAux.y = -cameraHeight + 0.5f;
+            transform.position = positionAux;
+
+        }
+        
+        /*
+        *verificar os clips mas com (clamp)
+        */
+
+        float cameraHeight = Camera.main.orthographicSize;
+        Vector3 positionAux = transform.position;
+        positionAux.y = Mathf.Clamp (positionAux.y, -cameraHeight + (paddleHeight * 0.5f ), cameraHeight - (paddleHeight * 0.5f));
+        transform.position = positionAux;
+
+  
+    }
+}
---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+    bool bolafoilancada = false;
+    float tempo = 0f;
+    float cameraHeight ;
+    int pontosesquerda = 0;
+    int pontosdireita = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+                if(Random.value < 0.5f)
+                {
+                    GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
+                }
+                else
+                {
+                    GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.left;
+                }
+                  */
+        //GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        esperar2s();
+
+
+
+       // Debug.Log(tempo);
+
+
+        cameraHeight = 7.5f;
+
+
+        if (transform.position.x >= cameraHeight) //bola toca parede da direita 
+        {
+            pontosesquerda += 1; //somar um ponto a pontuação atual do jogador 1
+            Debug.Log("A pontuação é:");
+            Debug.Log("1º Jogador " + pontosesquerda + "-" + pontosdireita + " 2º Jogador"); //imrpimir na consola pontuação
+            bolafoilancada = false; //para voltar a lançar a bola
+
+
+            transform.position = Vector3.zero;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            tempo = 0f;
+            esperar2s();
+
+        }
+        else if (transform.position.x <= -cameraHeight) //bola toca parede da esquerda
+        {
+            pontosdireita += 1; //somar um ponto a pontuação atual do jogador 2
+            Debug.Log("O resultado é");
+            Debug.Log("1º Jogador " + pontosesquerda + "-" + pontosdireita + " 2º Jogador"); //imrpimir na consola pontuação
+            bolafoilancada = false; //para voltar a lançar a bola
+
+            transform.position = Vector3.zero;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            tempo = 0f;
+            esperar2s();
+        }
+
+    }
+
+    void esperar2s ()
+    {
+        if (bolafoilancada == false)
+        {
+            tempo = tempo + Time.deltaTime;
+
+            if (tempo >= 2)
+            {
+                GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitCircle;
+                bolafoilancada = true;
+            }
+        }
+
+    }
+}
+
+
