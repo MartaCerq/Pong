8be8d6e608b663adf310f89163f198150c9bd278
MartaCerq <79975159+MartaCerq@users.noreply.github.com>
2021-03-22 19:48:57 +0000

    Initial commit

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -8,9 +8,7 @@ public class ball : MonoBehaviour
     float velocidade = 5f;
     bool bolafoilancada = false;
     float tempo = 0f;
-    float cameraHeight ;
-    int pontosesquerda = 0;
-    int pontosdireita = 0;
+  
 
 
 
@@ -34,47 +32,8 @@ public class ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        esperar2s();
 
 
-
-       // Debug.Log(tempo);
-
-
-        cameraHeight = 7.5f;
-
-
-        if (transform.position.x >= cameraHeight) //bola toca parede da direita 
-        {
-            pontosesquerda += 1; //somar um ponto a pontuação atual do jogador 1
-            Debug.Log("A pontuação é:");
-            Debug.Log("1º Jogador " + pontosesquerda + "-" + pontosdireita + " 2º Jogador"); //imrpimir na consola pontuação
-            bolafoilancada = false; //para voltar a lançar a bola
-
-
-            transform.position = Vector3.zero;
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            tempo = 0f;
-            esperar2s();
-
-        }
-        else if (transform.position.x <= -cameraHeight) //bola toca parede da esquerda
-        {
-            pontosdireita += 1; //somar um ponto a pontuação atual do jogador 2
-            Debug.Log("O resultado é");
-            Debug.Log("1º Jogador " + pontosesquerda + "-" + pontosdireita + " 2º Jogador"); //imrpimir na consola pontuação
-            bolafoilancada = false; //para voltar a lançar a bola
-
-            transform.position = Vector3.zero;
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            tempo = 0f;
-            esperar2s();
-        }
-
-    }
-
-    void esperar2s ()
-    {
         if (bolafoilancada == false)
         {
             tempo = tempo + Time.deltaTime;
@@ -87,6 +46,6 @@ public class ball : MonoBehaviour
         }
 
     }
-}
+}    
 
 
